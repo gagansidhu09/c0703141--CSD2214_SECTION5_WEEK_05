eeea19bce38c9ed57a18b872af1bcb3be2b664d1
Gaganpreet Singh Sidhu <c0703141@mylambton.ca>
2019-02-07 12:01:03 -0500

    final commit

--- c0703141week5/js/main.js
+++ c0703141week5/js/main.js
@@ -1,1 +1,14 @@
-﻿// Your code here!
+﻿
+
+function arrays() {
+    var pens;
+    pens = new Array("red", "orange", "green", "blue", "purple");
+    markers = pens;
+    console.log(markers);
+    console.log(markers[0]);
+    for (i = 0; i < markers.length; i++) {
+        alert(markers[i]);
+
+    }
+    alert("Length of array is:" + markers.length);
+}
